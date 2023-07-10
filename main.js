e527172548d6104f1073df0e94d9c7547e002bec
Omar Elmoez <omarelmoez2000@gmail.com>
2023-07-11 00:07:23 +0300

    activate Search Function

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ let discountField = document.querySelector("#discount");
 let totalField = document.querySelector("#total");
 let countField = document.querySelector("#count");
 let categoryField = document.querySelector("#category");
+// Get Search
+let search_mood = "title";
+let searchField = document.querySelector("#search");
 
 for (let i = 0; i < inputFields.length; i++) {
   inputFields[i].addEventListener("input", () => {
@@ -40,20 +43,20 @@ let allProducts = [];
 if (localStorage.products)
   allProducts = JSON.parse(localStorage.getItem("products"));
 
-function ShowData() {
+function ShowData(arr = allProducts) {
   let tableInfo = "";
 
-  for (let i = 0; i < allProducts.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let row = `
         <tr>
           <td>${i + 1}</td>
-          <td>${allProducts[i].title}</td>
-          <td>${allProducts[i].price}</td>
-          <td>${allProducts[i].taxes}</td>
-          <td>${allProducts[i].ads}</td>
-          <td>${allProducts[i].discount}</td>
-          <td>${allProducts[i].total}</td>
-          <td>${allProducts[i].category}</td>
+          <td>${arr[i].title}</td>
+          <td>${arr[i].price}</td>
+          <td>${arr[i].taxes}</td>
+          <td>${arr[i].ads}</td>
+          <td>${arr[i].discount}</td>
+          <td>${arr[i].total}</td>
+          <td>${arr[i].category}</td>
           <td><button class='btn' id='update-btn' onclick ='updateProduct(${i})'>Update</button></td>
           <td><button class='btn' id='delete-btn' onclick='deleteProduct(${i})'>Delete</button></td>
         </tr>
@@ -62,7 +65,7 @@ function ShowData() {
   }
   tbody.innerHTML = tableInfo;
 
-  if (allProducts.length) {
+  if (arr.length) {
     // ====================================
     // This code will create a button every time you add a product
     // let deleteAll_btn = document.createElement("button");
@@ -73,7 +76,7 @@ function ShowData() {
     // ====================================
     document.querySelector(
       "#deleteAll-box"
-    ).innerHTML = `<button class='btn' onclick='deleteAllProducts()'>Delete All (${allProducts.length})</button>`;
+    ).innerHTML = `<button class='btn' onclick='deleteAllProducts()'>Delete All (${arr.length})</button>`;
   } else {
     document.querySelector("#deleteAll-box").innerHTML = "";
   }
@@ -117,6 +120,18 @@ function deleteAllProducts() {
   ShowData();
 }
 
+// There is another way to activate the Update function:
+// - create a variable to hold the (mood) of the programe. => let mood = 'create'; Global Variable.
+// - at line 186 => this is where we add new product.
+// - tho, when the mood is create, make the click event on the create_btn add new product.
+// - if(mood === 'create') {}
+// - if mood was 'update' or any thing except 'create'. => else {}
+// - take the (productInfo) and add it in the array at the (same) index of the product you want to update.
+// - Now, How we get the index of the desired product. => make a Global Variable called temp;
+// - when you click update button, it will pass the index to the function and then assign it to the temp variable.
+// - See the video Number 10.
+// - this way makes you don't need to create another button or use the data-attribute in HTML.
+
 function updateProduct(i) {
   let product = allProducts[i];
   // Show Information Of Product
@@ -125,10 +140,15 @@ function updateProduct(i) {
   taxesField.value = product.taxes;
   adsField.value = product.ads;
   discountField.value = product.discount;
-  calcTotal(priceField.value, taxesField.value, adsField.value, discountField.value);
+  calcTotal(
+    priceField.value,
+    taxesField.value,
+    adsField.value,
+    discountField.value
+  );
   // There is no need for count field => user just modify the exsiting product
   // countField.value = product.count || 1; => X
-  countField.style.display = 'none'
+  countField.style.display = "none";
   categoryField.value = product.category;
   // ==================================
 
@@ -141,12 +161,12 @@ function updateProduct(i) {
   scroll({
     top: 0,
     behavior: "smooth",
-  })
+  });
 }
 
 modify_btn.addEventListener("click", () => {
   create_btn.classList.remove("hide");
-  countField.style.display = 'block';
+  countField.style.display = "block";
   let positionInArr = modify_btn.dataset.productIndex;
   const productInfo = {
     title: titleField.value,
@@ -161,7 +181,7 @@ modify_btn.addEventListener("click", () => {
   allProducts.splice(positionInArr, 1, productInfo);
   localStorage.setItem("products", JSON.stringify(allProducts));
   ShowData();
-  Reset(); 
+  Reset();
 });
 
 create_btn.addEventListener("click", (e) => {
@@ -193,4 +213,31 @@ create_btn.addEventListener("click", (e) => {
 
   // ================= Showing Products  =================
   ShowData();
+});
+
+function setSearchMood(id) {
+  searchField.style.height = "35px";
+  searchField.focus();
+  searchField.value = "";
+  if (!searchField.value) ShowData();
+  searchField.placeholder = "search By Title";
+  if (id === "searchByCategory") {
+    search_mood = "category";
+    searchField.placeholder = "search By Category";
+  }
+}
+
+searchField.addEventListener("keyup", () => {
+  search_value = searchField.value;
+  const filterd_data = allProducts.filter(
+    // Don't use === because this needs the user to know the product title as it stored in database.
+    (item) => {
+      if (search_mood === "title") {
+        return item.title.toLowerCase().includes(search_value.toLowerCase());
+      } else {
+        return item.category.toLowerCase().includes(search_value.toLowerCase());
+      }
+    }
+  );
+  ShowData(filterd_data);
 });
